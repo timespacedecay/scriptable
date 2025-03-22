184963e0db1c011c9b21dc162e03c3cd44aec683
timespacedecay <github.sablefishes@get.lostinspace.me>
2025-03-22 11:31:31 -0700

    Version 4 - Caching and Updates by Nodman

--- Next F1 Race Schedule.js
+++ Next F1 Race Schedule.js
@@ -1,211 +1,313 @@
 // Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: flag-checkered;
 
-// Citation and thank yous:
-// F1 race data from the great project jolpica-f1, which took over where ergast left off. Check out that project here: https://github.com/jolpica/jolpica-f1
+// Acknowledgements:
+// F1 race data from the great project jolpica-f1, which took over where ergast left off. https://github.com/jolpica/jolpica-f1
+// Nodman for adding caching and ability to update the script. https://github.com/Nodman
+// 
 
-const now = new Date()
+// --------------------------------------------------
+// 1) Constants & Setup - DO NOT EDIT
+// --------------------------------------------------
+const SCRIPT_VERSION = "4.0";
+const DATA_URL = "https://api.jolpi.ca/ergast/f1/current/next.json";
+const RACE_IDX = 0;
+const now = new Date();
 
-const options = {
-	//version is "User-Agent"
-	version: "Scriptable: NextF1RaceSchedule/3.0",
-	dataUrl: "https://api.jolpi.ca/ergast/f1/current/next.json",
-	raceIdx: 0,
-	width: 170,
-	font:{
-		header:	["HiraginoSans-W7", 10],
-		title:	["HiraginoSans-W6", 9],
-		body:	["HiraginoSans-W4", 9]
-	},
-	// Edit this for column resize
-	padding:{
-		left:	-4,
-		right:	-4
-	},
-	spaceBetweenRows: 2,
-	spaceBetweenColumns: 0,
-	//adjustable refresh time (less than 60 is ignored)
+// Paths and file manager
+const scriptPath = module.filename;
+const fm = FileManager.local();
+
+// If you want to store the script in iCloud, you can do:
+// if (fm.isFileStoredIniCloud(scriptPath)) fm = FileManager.iCloud();
+
+const UPDATE_URL = "https://raw.githubusercontent.com/timespacedecay/scriptable/refs/heads/main/Next%20F1%20Race%20Schedule.js";
+
+// Widget layout options
+let options = {
+    width: 170,
+    font: {
+        header: ["HiraginoSans-W7", 10],
+        title: ["HiraginoSans-W6", 9],
+        body: ["HiraginoSans-W4", 9]
+    },
+    padding: {
+        left: -4,
+        right: -4
+    },
+    spaceBetweenRows: 2,
+    spaceBetweenColumns: 0,
+    //adjustable refresh time (less than 60 is ignored)
 	refreshLimitInMinutes: 60
+};
+
+// --------------------------------------------------
+// 2) Build the widget
+// --------------------------------------------------
+const widget = await createWidget();
+
+// --------------------------------------------------
+// 3) Show menu so user can choose what to do
+// --------------------------------------------------
+if (!(config.runsInWidget)) {
+const menu = new Alert();
+menu.title = "F1 Race Schedule";
+menu.message = "Choose an action:";
+menu.addAction("Preview Small");
+menu.addAction("Preview Medium");
+menu.addAction("Preview Large");
+menu.addAction("Set Widget");
+menu.addAction("Update Script");
+menu.addCancelAction("Cancel");
+
+const selection = await menu.presentAlert();
+
+switch (selection) {
+    case 0:
+        // Preview small
+        await widget.presentSmall();
+        break;
+    case 1:
+        // Preview medium
+        await widget.presentMedium();
+        break;
+    case 2:
+        // Preview large
+        await widget.presentLarge();
+        break;
+    case 3:
+        // Set as the widget for the Home Screen
+        Script.setWidget(widget);
+        Script.complete();
+        return;
+    case 4:
+        // Update script code
+        await updateScript();
+        break;
+    default:
+        // Cancel
+        break;
 }
 
-//save cached data to 'Scriptable/f1RaceData/schedule.txt'
-
-let FM
-
-try { FM = FileManager.iCloud() }
-catch {FM = FileManager.local()}
-
-if(!FM.fileExists(FM.documentsDirectory()+'/f1RaceData/'))
-	FM.createDirectory(FM.documentsDirectory()+'/f1RaceData/')
-let filePath=FM.documentsDirectory()+'/f1RaceData/schedule.txt'
-
-
-//// for testing// 
-// const dataUrl = "https://api.jolpi.ca/ergast/f1/current/races.json";// 
-// const raceIdx = 6
-
-function finished(time){	return time<now?.5:1	}
-
-let widget = await createWidget();
-Script.setWidget(widget);
-//// for testing
-widget.presentAccessoryRectangular(); 
+// If you didn't choose "Set Widget & Exit", let's just end:
 Script.complete();
-
-//Cache data
-function cacheRaceData(dataObject){
-    console.log('caching new race data...')
-    dataObject.lastUpdate = now
-    FM.writeString(filePath, JSON.stringify(dataObject))
+} else {
+    // Set as the widget for the Home Screen
+        Script.setWidget(widget);
+        Script.complete();
 }
-
-//API call limiter
-//uses getTime()/1000/60 to check if it's been at least 60 minutes since last update
-async function getRaceData(){
-	let temp, _rlim = options.refreshLimitInMinutes<60?60:options.refreshLimitInMinutes
-	if(FM.fileExists(filePath)){
-		temp = FM.downloadFileFromiCloud(filePath)
-		temp = FM.readString(filePath)
-		temp = JSON.parse(temp)
-		//if time elapsed is less than 1 hour, use cache
-		if(Math.abs((new Date(temp.lastUpdate)).getTime()/1000/60 - now.getTime()/1000/60)<_rlim){
-			//console.log('using cached data...')
-			return temp
-		}
-	}
-
-	//if time elapsed is 1+ hours, use API
-	//console.log('calling API...')
-	temp = new Request(options.dataUrl)
-	//added a header to show updated widget users
-	temp.headers = {"User-Agent":options.version}
-	temp = await temp.loadJSON()
-	cacheRaceData(temp)
-	return temp
-}
-
-async function formatSessionDay(sessionDay) {
-    var options = { weekday: 'short' };
-    return sessionDay.toLocaleDateString('en-US', options);
-}
-
-async function formatSessionDate(sessionDate) {
-    var options = { month: 'numeric', day: 'numeric' };
-    return sessionDate.toLocaleDateString('en-US', options);
-}
-
-async function formatSessionTime(sessionTime) {
-    var options = { hour12: false, hour: '2-digit', minute:'2-digit' };
-    return sessionTime.toLocaleTimeString('en-US', options);
-}
-
+/** 
+ * Creates the main F1 schedule widget.
+ */
 async function createWidget() {
-	const widget = new ListWidget();
-	const data = await getRaceData()	//await new Request(options.dataUrl).loadJSON();
-	const race = data.MRData.RaceTable.Races[options.raceIdx]
-	const raceDateTime = new Date(`${race.date}T${race.time}`)
-	const fp1 = race.FirstPractice
-	const fp1DateTime = new Date(`${fp1.date}T${fp1.time}`)
-	const quali = race.Qualifying
-	const qualiDateTime = new Date(`${quali.date}T${quali.time}`)
+    const w = new ListWidget();
+    const data = await getData(); // uses caching + headers
+    const race = data.MRData.RaceTable.Races[RACE_IDX];
+    const raceDateTime = new Date(`${race.date}T${race.time}`);
+    const fp1 = race.FirstPractice;
+    const fp1DateTime = new Date(`${fp1.date}T${fp1.time}`);
+    const quali = race.Qualifying;
+    const qualiDateTime = new Date(`${quali.date}T${quali.time}`);
 
-	let sprintQ, fp2sprintQDateTime, sprint, fp3sprintDateTime, fp2, fp3, sprintOrSP, isSprint = Object.hasOwn(race,'Sprint')
+    let sprintOrSP, isSprint = Object.hasOwn(race, "Sprint");
 
-	let dateTime = []
-		dateTime[0] = {
-			title: 'FP1',
-			day:	await formatSessionDay(fp1DateTime),
-			date:	await formatSessionDate(fp1DateTime),
-			time:	await formatSessionTime(fp1DateTime),
-			raw:	fp1DateTime
-		}
+    let dateTime = [];
+    dateTime[0] = {
+        title: "FP1",
+        day: await formatSessionDay(fp1DateTime),
+        date: await formatSessionDate(fp1DateTime),
+        time: await formatSessionTime(fp1DateTime),
+        raw: fp1DateTime
+    };
 
-		sprintOrSP = isSprint?race.SprintQualifying:race.SecondPractice
-		fp2sprintQDateTime = new Date(`${sprintOrSP.date}T${sprintOrSP.time}`)
+    sprintOrSP = isSprint ? race.SprintQualifying : race.SecondPractice;
+    const fp2sprintQDateTime = new Date(`${sprintOrSP.date}T${sprintOrSP.time}`);
+    dateTime[1] = {
+        title: isSprint ? "SQ" : "FP2",
+        day: await formatSessionDay(fp2sprintQDateTime),
+        date: await formatSessionDate(fp2sprintQDateTime),
+        time: await formatSessionTime(fp2sprintQDateTime),
+        raw: fp2sprintQDateTime
+    };
 
-		dateTime[1] = {
-			title:	isSprint?'SQ':'FP2',
-			day:	await formatSessionDay(fp2sprintQDateTime),
-			date:	await formatSessionDate(fp2sprintQDateTime),
-			time:	await formatSessionTime(fp2sprintQDateTime),
-			raw:	fp2sprintQDateTime
-		}
+    sprintOrSP = isSprint ? race.Sprint : race.ThirdPractice;
+    const fp3sprintDateTime = new Date(`${sprintOrSP.date}T${sprintOrSP.time}`);
+    dateTime[2] = {
+        title: isSprint ? "SPR" : "FP3",
+        day: await formatSessionDay(fp3sprintDateTime),
+        date: await formatSessionDate(fp3sprintDateTime),
+        time: await formatSessionTime(fp3sprintDateTime),
+        raw: fp3sprintDateTime
+    };
 
-		sprintOrSP = isSprint?race.Sprint:race.ThirdPractice
-		fp3sprintDateTime = new Date(`${sprintOrSP.date}T${sprintOrSP.time}`)
+    dateTime[3] = {
+        title: "Quali",
+        day: await formatSessionDay(qualiDateTime),
+        date: await formatSessionDate(qualiDateTime),
+        time: await formatSessionTime(qualiDateTime),
+        raw: qualiDateTime
+    };
 
-		dateTime[2]= {
-			title:	isSprint?'SPR':'FP3',
-			day:	await formatSessionDay(fp3sprintDateTime),
-			date:	await formatSessionDate(fp3sprintDateTime),
-			time:	await formatSessionTime(fp3sprintDateTime),
-			raw:	fp3sprintDateTime
-		}
+    dateTime[4] = {
+        title: "Race",
+        day: await formatSessionDay(raceDateTime),
+        date: await formatSessionDate(raceDateTime),
+        time: await formatSessionTime(raceDateTime),
+        raw: raceDateTime
+    };
 
-		dateTime[3] = {
-			title:	'Quali',
-			day:	await formatSessionDay(qualiDateTime),
-			date:	await formatSessionDate(qualiDateTime),
-			time:	await formatSessionTime(qualiDateTime),
-			raw:	qualiDateTime
-		}
-		dateTime[4] = {
-			title:	'Race',
-			day:	await formatSessionDay(raceDateTime),
-			date:	await formatSessionDate(raceDateTime),
-			time:	await formatSessionTime(raceDateTime),
-			raw:	raceDateTime
-		}
+    // HEADER
+    const headerStack = w.addStack();
+    const headerText = race.raceName.toUpperCase();
+    const headerCell = headerStack.addStack();
+    headerCell.size = new Size(options.width, 0);
+    headerCell.addSpacer();
 
-	const headerStack = widget.addStack()
-	const headerText = race.raceName.toUpperCase()
-	const headerCell = headerStack.addStack()
-		  //headerCell.backgroundColor = HEADER_COLOR
-		  headerCell.size = new Size(options.width,0)
-		  headerCell.addSpacer()
+    const textElement = headerCell.addText(headerText);
+    textElement.font = new Font(...options.font.header);
+    textElement.minimumScaleFactor = 0.5;
+    textElement.lineLimit = 1;
 
-		  const textElement = headerCell.addText(headerText)
-				textElement.font = new Font(...options.font.header)
-				textElement.minimumScaleFactor = 0.5
-				textElement.lineLimit = 1
+    headerCell.addSpacer();
+    w.addSpacer(options.spaceBetweenRows);
 
-		  headerCell.addSpacer()
+    // BODY
+    let body = w.addStack();
+    body.size = new Size(options.width, 0);
 
-	widget.addSpacer(options.spaceBetweenRows)
+    for (let column = 0; column < dateTime.length; column++) {
+        let currentColumn = body.addStack();
+        currentColumn.layoutVertically();
+        currentColumn.setPadding(0, options.padding.left, 0, options.padding.right);
 
-	let body = widget.addStack()
-		//change: width,height (0 = auto size)
-		body.size = new Size(options.width,0)
-		//body.cornerRadius = 1
+        for (let row in dateTime[column]) {
+            if (row === "raw") continue;
+            let currentCell = currentColumn.addStack();
+            currentCell.addSpacer();
 
-	for(let column=0; column<dateTime.length; column++){
-		let currentColumn = body.addStack()
-			currentColumn.layoutVertically()
+            let cellText = currentCell.addText(dateTime[column][row]);
+            cellText.font = (row === "title")
+                ? new Font(...options.font.title)
+                : new Font(...options.font.body);
 
-			//adjust column padding
-			currentColumn.setPadding(0,options.padding.left,0,options.padding.right)
+            // Gray out past sessions
+            cellText.textOpacity = finished(dateTime[column].raw);
+            cellText.lineLimit = 1;
+            cellText.minimumScaleFactor = 0.5;
 
-		for(let row in dateTime[column]){
-			if(row=='raw') continue
-			let currentCell = currentColumn.addStack()
-				//left side spacer for Text
-				currentCell.addSpacer()
-			let cellText = currentCell.addText(dateTime[column][row])
-				//if row==0, use title font, else use body font
-				cellText.font = row=='title'?new Font(...options.font.title):new Font(...options.font.body)
-				// cellText.textColor = Color.white()
-				cellText.lineLimit = 1
-				cellText.minimumScaleFactor = .5
-				cellText.textOpacity = finished(dateTime[column].raw)
-			//right side spacer for Text
-			currentCell.addSpacer()
-            //space between title, fp2, etc
-            currentColumn.addSpacer(options.spaceBetweenRows)
-		}
-        //space bewtween columns
-		currentColumn.addSpacer(options.spaceBetweenColumns)
-	}
+            currentCell.addSpacer();
+            currentColumn.addSpacer(options.spaceBetweenRows);
+        }
+        currentColumn.addSpacer(options.spaceBetweenColumns);
+    }
 
-	return widget;
+    return w;
+}
+
+/**
+ * Lower opacity for past times, full for future times.
+ */
+function finished(time) {
+    return time < now ? 0.5 : 1;
+}
+
+/**
+ * Returns data from cache if <1hr old, otherwise fetch with custom headers.
+ */
+async function getData() {
+    const cachePath = fm.joinPath(fm.cacheDirectory(), "f1DataCache.json");
+    const nowMs = Date.now();
+    let _rlim = options.refreshLimitInMinutes<60?60:options.refreshLimitInMinutes
+    // Try reading from cache
+    if (fm.fileExists(cachePath)) {
+        try {
+            const cached = JSON.parse(fm.readString(cachePath));
+            const ageMs = nowMs - cached.timestamp;
+            if (ageMs < _rlim) { // 1 hour or more
+                console.log("Using cached data");
+                return cached.data;
+            } else {
+                console.log("Cache too old, need fresh data");
+            }
+        } catch (e) {
+            console.log("Error reading cache, will fetch fresh data.");
+        }
+    }
+
+    // Otherwise, fetch fresh data
+    const req = new Request(DATA_URL);
+    req.headers = {
+        "User-Agent": `Scriptable: NextF1RaceScheduleHomeScreen/${SCRIPT_VERSION}`
+    };
+    const data = await req.loadJSON();
+
+    // Cache it
+    fm.writeString(
+        cachePath,
+        JSON.stringify({
+            timestamp: nowMs,
+            data
+        })
+    );
+    console.log("Fetched fresh data from API");
+    return data;
+}
+
+/**
+ * Format day (e.g. "Mon")
+ */
+async function formatSessionDay(sessionDay) {
+    return sessionDay.toLocaleDateString("en-US", { weekday: "short" });
+}
+
+/**
+ * Format date (e.g. "4/15")
+ */
+async function formatSessionDate(sessionDate) {
+    return sessionDate.toLocaleDateString("en-US", { month: "numeric", day: "numeric" });
+}
+
+/**
+ * Format time (e.g. "14:00")
+ */
+async function formatSessionTime(sessionTime) {
+    return sessionTime.toLocaleTimeString("en-US", { hour12: false, hour: "2-digit", minute: "2-digit" });
+}
+
+/**
+ * ---------------------------------------------------------------
+ * "Update Code" - Overwrite the current script with the one
+ * hosted at UPDATE_URL.
+ * ---------------------------------------------------------------
+ */
+async function updateScript() {
+    const alert = new Alert();
+    const scriptName = scriptPath.split("/").pop();
+
+    alert.title = `Update "${scriptName}" code?`;
+    alert.message = "This will overwrite your local changes!";
+    alert.addCancelAction("No");
+    alert.addDestructiveAction("Yes, overwrite");
+    const choice = await alert.present();
+
+    if (choice === -1) {
+        // User tapped "No"
+        return;
+    }
+
+    let updateMessage;
+    try {
+        const req = new Request(UPDATE_URL);
+        const newCode = await req.loadString();
+        fm.writeString(scriptPath, newCode);
+        updateMessage = "Code updated. Close & reopen the script to see changes.";
+    } catch (error) {
+        updateMessage = `Update failed. ${error}`;
+    }
+
+    const resultAlert = new Alert();
+    resultAlert.title = "Update code";
+    resultAlert.message = updateMessage;
+    resultAlert.addCancelAction("OK");
+    await resultAlert.present();
 }
